cc80a05b208b46ad04c8988ea1f5defe8c62ade0
Andrey Prokopenko <dr.watson92ap@gmail.com>
2020-11-20 14:27:52 +0300

    apple sign in fix

--- OAuth/FirebaseOAuthUI/FUIOAuth.m
+++ OAuth/FirebaseOAuthUI/FUIOAuth.m
@@ -22,6 +22,7 @@
 #import <FirebaseUI/FUIAuthBaseViewController.h>
 #import <FirebaseUI/FUIAuthBaseViewController_Internal.h>
 #import "FUIAuthErrorUtils.h"
+@import CommonCrypto;
 
 /** @var kTableName
     @brief The name of the strings table to search for localized strings.
@@ -127,6 +128,38 @@ NS_ASSUME_NONNULL_BEGIN
     }
   }
   return self;
+}
+
+- (NSString *)randomNonce:(NSInteger)length {
+  NSAssert(length > 0, @"Expected nonce to have positive length");
+  NSString *characterSet = @"0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._";
+  NSMutableString *result = [NSMutableString string];
+  NSInteger remainingLength = length;
+
+  while (remainingLength > 0) {
+    NSMutableArray *randoms = [NSMutableArray arrayWithCapacity:16];
+    for (NSInteger i = 0; i < 16; i++) {
+      uint8_t random = 0;
+      int errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random);
+      NSAssert(errorCode == errSecSuccess, @"Unable to generate nonce: OSStatus %i", errorCode);
+
+      [randoms addObject:@(random)];
+    }
+
+    for (NSNumber *random in randoms) {
+      if (remainingLength == 0) {
+        break;
+      }
+
+      if (random.unsignedIntValue < characterSet.length) {
+        unichar character = [characterSet characterAtIndex:random.unsignedIntValue];
+        [result appendFormat:@"%C", character];
+        remainingLength--;
+      }
+    }
+  }
+
+  return result;
 }
 
 + (FUIOAuth *)twitterAuthProvider {
@@ -241,6 +274,9 @@ NS_ASSUME_NONNULL_BEGIN
     if (@available(iOS 13.0, *)) {
       ASAuthorizationAppleIDRequest *request = [[[ASAuthorizationAppleIDProvider alloc] init] createRequest];
       request.requestedScopes = @[ASAuthorizationScopeFullName, ASAuthorizationScopeEmail];
+        NSString *nonce = [self randomNonce:32];
+          self.currentNonce = nonce;
+        request.nonce = [self stringBySha256HashingString:nonce];
       ASAuthorizationController* controller = [[ASAuthorizationController alloc] initWithAuthorizationRequests:@[request]];
       controller.delegate = self;
       controller.presentationContextProvider = self;
@@ -283,6 +319,18 @@ NS_ASSUME_NONNULL_BEGIN
       }
     }];
   }
+}
+
+- (NSString *)stringBySha256HashingString:(NSString *)input {
+  const char *string = [input UTF8String];
+  unsigned char result[CC_SHA256_DIGEST_LENGTH];
+  CC_SHA256(string, (CC_LONG)strlen(string), result);
+
+  NSMutableString *hashed = [NSMutableString stringWithCapacity:CC_SHA256_DIGEST_LENGTH * 2];
+  for (NSInteger i = 0; i < CC_SHA256_DIGEST_LENGTH; i++) {
+    [hashed appendFormat:@"%02x", result[i]];
+  }
+  return hashed;
 }
 
 - (void)signOut {
